14d17618a7c6f2c40214b4656cb2f77b84d94dee
Mustafa Haddara <mhaddara@pagerduty.com>
2022-04-05 15:38:06 -0400

    setup runners/destroyers

--- app-b/src/index.js
+++ app-b/src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const RUNNER_KEY = `app-b_runner`;
+const DESTROYER_KEY = `app-b_destroyer`;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+window[RUNNER_KEY] = (opts, targetElementId) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById(targetElementId)
+  );
+}
+
+window[DESTROYER_KEY] = (opts, targetElementId) => {
+  ReactDOM.unmountComponentAtNode(document.getElementById(targetElementId));
+};
