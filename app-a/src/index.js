4caa5ab3e48620343f7df950422ff68ee0ea8f8b
Mustafa Haddara <mhaddara@pagerduty.com>
2022-04-05 16:51:49 -0400

    init datadog rum

--- app-a/src/index.js
+++ app-a/src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { datadogRum } from '@datadog/browser-rum';
 import App from './App';
+
+datadogRum.init({
+  applicationId: '58ba5b56-bd28-4925-b4a8-a859ef8f933e',
+  clientToken: 'pub7dc1dbfb0e790a3fc1c01d4b2858f54b',
+  site: 'datadoghq.com',
+  service:'mustafa-test-app-a',
+  sampleRate: 100,
+  trackInteractions: true
+});
 
 const RUNNER_KEY = `app-a_runner`;
 const DESTROYER_KEY = `app-a_destroyer`;
--- app-b/src/index.js
+++ app-b/src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { datadogRum } from '@datadog/browser-rum';
 import App from './App';
+
+datadogRum.init({
+  applicationId: '48e9ae22-1484-47b2-8c04-9a0eba9cde64',
+  clientToken: 'pub272d2b3cad133fd24eecf58f8e7045cf',
+  site: 'datadoghq.com',
+  service:'mustafa-test-app-b',
+  sampleRate: 100,
+  trackInteractions: true
+});
 
 const RUNNER_KEY = `app-b_runner`;
 const DESTROYER_KEY = `app-b_destroyer`;
